bc990d1add4f0cbe50497a610cf1283cd7680472
saimouli <ksaimouli@gmail.com>
2018-10-16 11:36:17 -0700

    [app][VisionModule][Task 2.4]: Implemented method definitions until isolateLanes

--- app/VisionModule.cpp
+++ app/VisionModule.cpp
@@ -80,13 +80,17 @@ cv::Mat VisionModule::getTopView(const cv::Mat& frame) {
 }
 
 void VisionModule::createMask(const cv::Mat& topView) {
-
+  cv::Mat yellowMask;
+  cv::Mat whiteMask;
+  cv::Mat topViewHSV;
+  cvtColor(topView, topViewHSV, CV_BGR2HSV);
+  // Thresholding for the lanes
+  inRange(topViewHSV, cv::Scalar(0, 65, 104), cv::Scalar(87, 255, 255),
+          yellowMask);
+  inRange(topViewHSV, cv::Scalar(0, 0, 200, 0), cv::Scalar(180, 255, 255, 0),
+          whiteMask);
+  bitwise_or(whiteMask, yellowMask, mask);
 }
-
-void VisionModule::edgeFilter(cv::Mat& processFrame) {
-
-}
-
 
 std::vector<cv::Vec4i> VisionModule::houghLinesDetect(cv::Mat& processFrames) {
   std::vector<cv::Vec4i> lines;
@@ -94,7 +98,25 @@ std::vector<cv::Vec4i> VisionModule::houghLinesDetect(cv::Mat& processFrames) {
 }
 
 void VisionModule::getHistogramPeaks() {
+  cv::Point leftPeak;
+  cv::Point rightPeak;
+  double minVal, maxVal;
+  cv::Point minLoc;
+  cv::Mat leftHalf = mask(cv::Range(0, mask.rows),
+                          cv::Range(0, mask.cols / 2 - 1));
+  cv::Mat rightHalf = mask(cv::Range(0, mask.rows),
+                           cv::Range(mask.cols / 2, mask.cols - 1));
 
+  // Histogram calculation for left and right halves
+  cv::Mat histLeft, histRight;
+  reduce(leftHalf, histLeft, 0, CV_REDUCE_SUM, CV_32S);
+  reduce(rightHalf, histRight, 0, CV_REDUCE_SUM, CV_32S);
+
+  minMaxLoc(histLeft, &minVal, &maxVal, &minLoc, &leftPeak);
+  minMaxLoc(histRight, &minVal, &maxVal, &minLoc, &rightPeak);
+  rightPeak.x += mask.cols / 2;
+  peaks.push_back(leftPeak);
+  peaks.push_back(rightPeak);
 }
 
 std::vector<cv::Point> VisionModule::returnPeaks() {
@@ -103,7 +125,42 @@ std::vector<cv::Point> VisionModule::returnPeaks() {
 
 std::vector<cv::Point> VisionModule::isolateLane(cv::Point peak,
                                                  const cv::Mat& topView) {
-  std::vector<cv::Point> boxCentroid;
+   std::vector<cv::Point> boxCentroid;
+  boxCentroid.clear();
+  for (int i = 0; i < numBoxes; i++) {
+    int boxHeight = mask.rows / numBoxes;
+    int bottomY = mask.rows - i * boxHeight;
+    int topY = mask.rows - (i + 1) * boxHeight;
+
+    // Draw the rectangles
+    cv::rectangle(topView, cv::Point(peak.x - boxMargin, topY),
+                  cv::Point(peak.x + boxMargin, bottomY),
+                  cv::Scalar(0, 255, 255), 3);
+
+    // Adjust the base according to number of white pixels
+    cv::Mat ROI = mask(cv::Range(topY, bottomY),
+                       cv::Range(peak.x - boxMargin, peak.x + boxMargin));
+
+    // Find number of white pixels
+    cv::Mat whitePixelsLoc;
+    findNonZero(ROI, whitePixelsLoc);
+
+    // check if number of white pixels is greater than 100 for left line
+    if (whitePixelsLoc.rows > 100) {
+      // Find average of x locations
+      pixelLocSum = 0;
+      for (int j = 0; j < whitePixelsLoc.rows; j++) {
+        whitePixelsLoc.at<cv::Point>(j).x += (peak.x - boxMargin);
+        pixelLocSum += whitePixelsLoc.at<cv::Point>(j).x;
+      }
+      int average;
+      average = static_cast<int>(pixelLocSum / whitePixelsLoc.rows);
+      peak.x = average;
+    }
+    // Store the left and right box centroids
+    boxCentroid.push_back(cv::Point(peak.x, (topY + bottomY) / 2));
+  }
+  imshow("top view", topView);
   return boxCentroid;
 }
 
