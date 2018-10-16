f372f712f6d8b4aa7d694d4b57e1e9f18bb067e5
saimouli <ksaimouli@gmail.com>
2018-10-16 11:42:57 -0700

    [app][FrameParser][Task 2.4]: Implemented extractFrames method

--- app/FrameParser.cpp
+++ app/FrameParser.cpp
@@ -40,8 +40,67 @@ FrameParser::~FrameParser() {
 }
 
 int FrameParser::extractFrame(UserInterface interface) {
-return 0;
-}
+  VisionModule process;
+  std::string defaultChoice;
+  defaultChoice = interface.returnDefaultChoice();
+  if (defaultChoice == "n" || defaultChoice == "N") {
+    interface.getUserChoice();
+    interface.getInputLocation();
+  }
+
+  int choice;
+  choice = interface.returnUserChoice();
+
+  std::string fileName;
+  int cID;
+
+  // image input format
+  if (choice == 0) {
+    fileName = interface.returnInputLocation();
+    frame = cv::imread(fileName);
+    // check for vaild input
+    if (!frame.data) {
+      std::cout << "Could not open or find the image" << std::endl;
+      return -1;
+    }
+    frame = process.laneDetection(frame);
+    interface.displayLanes(frame);
+
+    std::cout << "To quit press any keystroke in the window" << std::endl;
+    // Wait for a keystroke in the window
+    cv::waitKey(0);
+    std::cout << "Thanks for using Vision Module app! " << std::endl;
+    return 0;
+
+  } else if (choice == 1) {
+    // video input format
+    fileName = interface.returnInputLocation();
+    cap = cv::VideoCapture(fileName);
+  } else if (choice == 2) {
+    cID = interface.returnCameraID();
+    cap = cv::VideoCapture(cID);
+  }
+  if (!cap.isOpened()) {
+    std::cout << "Error opening video stream or file" << std::endl;
+    return -1;
+  }
+  while (cap.read(frame)) {
+    if (frame.empty()) {
+      break;
+    }
+    frame = process.laneDetection(frame);
+    interface.displayLanes(frame);
+
+    // Press  ESC on keyboard to exit
+    char c = static_cast<char>(cv::waitKey(25));
+    if (c == 27)
+      break;
+  }
+  std::cout << "Thanks for using Vision Module app! " << std::endl;
+  cap.release();
+  cv::destroyAllWindows();
+  return 0;
+  }
 
 
 
