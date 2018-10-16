42103e07cc7eac575344d18ccb81cfed57ff4aa0
Adarsh Jagan S <adarshjagan3895@gmail.com>
2018-10-16 12:33:14 -0400

    [include][VisionModule][Task 2.1]: Included more and better methods for implementation.

--- include/VisionModule.h
+++ include/VisionModule.h
@@ -1,36 +1,4 @@
-/******************************************************************************
- *  MIT License
- *
- *  Copyright (c) 2018 Saimouli Katragadda, Adarsh Jagan Sathyamoorthy
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- *******************************************************************************/
-
-/**
- * @file      VisionModule.h
- * @author    Saimouli Katragadda (saimouli)
- * @copyright MIT License
- * @brief     VisionModule Class declaration
- * @detail    Declared functions Class to denoise frames, apply homography,
- *            find hough lines, and find heading angle
- */
-
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -41,10 +9,6 @@
 #ifndef INCLUDE_VISIONMODULE_H_
 #define INCLUDE_VISIONMODULE_H_
 
-using cv::Mat;
-using cv::Vec4i;
-using std::vector;
-
 /**
  * @brief Class VisionModule
  * The following class VisionModule gets the frames from FrameParser class
@@ -54,17 +18,59 @@ using std::vector;
 class VisionModule {
  private:
   /**
-   * @brief Container to store hough lines
+   * @brief Stores distortion parameters
    */
-  std::vector<cv::Vec4i> lines;
+  cv::Mat distortionCoeff =
+      (cv::Mat_<double>(1, 5) << -2.42565104e-01, -4.77893070e-02, -1.31388084e-03, -8.79107779e-05, 2.20573263e-02);
+  /**
+   * @brief Stores camera matrix parameters
+   */
+  cv::Mat cameraMatrix =
+      (cv::Mat_<double>(3, 3) << 1.15422732e+03, 0.00000000e+00, 6.71627794e+02, 0.00000000e+00, 1.14818221e+03, 3.86046312e+02, 0, 0, 1);
+  /**
+   * @brief Container to store undistored frames
+   */
+  cv::Mat undistortedFrame;
+  /**
+   * @brief Container to store masked frames
+   */
+  cv::Mat mask;
+  /**
+   * @brief Container to store histogram peak values
+   */
+  std::vector<cv::Point> peaks;
+  /**
+   * @brief Stores histogram box numbers
+   */
+  int numBoxes = 9;
+  /**
+   * @brief Stores histogram box margins numbers
+   */
+  int boxMargin = 100;
+  /**
+   * @brief variable to all the pixel sum in the histogram
+   */
+  int pixelLocSum = 0;
+  /**
+   * @brief variable to store vector of source points used for cropping
+   */
+  std::vector<cv::Point2f> srcPoints = { cv::Point2f(581, 477), cv::Point2f(
+      699, 477), cv::Point2f(896, 675), cv::Point2f(384, 675) };
+  /**
+   * @brief variable to store vector of distance points used for cropping
+   */
+  std::vector<cv::Point2f> dstPoints = { cv::Point2f(384, 0), cv::Point2f(896,
+                                                                          0),
+      cv::Point2f(896, 720), cv::Point2f(384, 720) };
+  /**
+   * @brief container to store the matrix for perspective transform
+   */
+  std::vector<cv::Mat> perspectiveMatrices;
   /**
    * @brief Container to store heading angle
    */
   double headingAngle;
-  /**
-   * @brief Container to store frames as they are being processed by functions
-   */
-  cv::Mat processFrame;
+
 
  public:
   /**
@@ -73,12 +79,30 @@ class VisionModule {
    * @return none
    */
   VisionModule();
+
   /**
    * @brief destructor VisionModule
    * @param none
    * @return none
    */
   ~VisionModule();
+
+  /**
+   * @brief Function returnCameraMatrix
+   * @param none
+   * @return cameraMatrix of type cv::Mat
+   * The following function will return the cameraMatrix private variable
+   */
+  cv::Mat returnCameraMatrix();
+
+  /**
+   * @brief Function returnDistortionCoeff
+   * @param none
+   * @return distortionCoeff of type cv::Mat
+   * The following function will return the distortionCoeff private variable
+   */
+  cv::Mat returnDistortionCoeff();
+
   /**
    * @brief Function unDistortImage
    * @param processFrame of type cv::Mat
@@ -86,38 +110,66 @@ class VisionModule {
    * The following function will undistort the given image using the
    * datasets given calibration matrix
    */
-  void unDistrortImage(&cv::Mat processFrame);
+  void unDistortImage(const cv::Mat& frame);
+
   /**
-   * @brief Function preProcess
-   * @param processFrame of type cv::Mat
+   * @brief Function smoothenImage
+   * @param none
    * @return none
    * The following function will denoise the image by applying gaussian filter
-   * and crop at ROI of the image
    */
-  void preProcess(&cv::Mat processFrame);
+  void smoothenImage();
+
   /**
-   * @brief Function perspectiveTransform
-   * @param processFrame of type cv::Mat
+   * @brief Function returnUndistortedFrame
+   * @param none
+   * @return undistortedFrame of type cv::Mat
+   * The following function returns the undistorted image stored as private data member
+   */
+  cv::Mat returnUndistortedFrame();
+
+  /**
+   * @brief Function computePerspectiveMatrices
+   * @param none
    * @return none
    * The following function will apply homography to the image
    * to get the birds eye
    */
-  void perspectiveTransform(&cv::Mat processFrame);
+  void computePerspectiveMatrices();
+
+  /**
+   * @brief Function returnPerspectiveMatrices
+   * @param none
+   * @return perspectiveMatrices of type std::vector<cv::Mat>
+   * The following function returns the computed perspective matrices private data member
+   */
+  std::vector<cv::Mat> returnPerspectiveMatrices();
+
+  /**
+   * @brief Function getTopView
+   * @param Frame of type cv::Mat
+   * @return frame of type cv::Mat
+   * The following function will get the birds view of the image
+   */
+  cv::Mat getTopView(const cv::Mat& frame);
+
+  /**
+   * @brief Function createMask
+   * @param frame of type cv::Mat
+   * @return none
+   * The following function will create a mask from the image
+   */
+  void createMask(const cv::Mat& frame);
+
   /**
    * @brief Function edgeFilter
-   * @param processFrame of type cv::Mat
+   * @param frame of type cv::Mat
    * @return none
-   * The following function will apply edge filter to the processed image
+   * The following function will detect edges in images
    */
+
   void edgeFilter(&cv::Mat processFrame);
-  /**
-   * @brief Function getHistogram
-   * @param processFrame of type cv::Mat
-   * @return none
-   * The following function will apply a histogram and finds a region where the white
-   * threshold values are high
-   */
-  void getHistogram(&cv::Mat processFrame);
+
   /**
    * @brief Function houghLinesDetect
    * @param processFrame of type cv::Mat
@@ -126,14 +178,52 @@ class VisionModule {
    * processed image
    */
   vector<cv::Vec4i> houghLinesDetect(&cv::Mat processFrames);
+
+  /**
+   * @brief Function getHistogramPeaks
+   * @param none
+   * @return none
+   * The following function will apply a histogram and finds a region where the white
+   * threshold values are high
+   */
+  void getHistogramPeaks();
+
+  /**
+   * @brief Function returnPeaks
+   * @param none
+   * @return peaks of type std::vector<cv::Point>
+   * The following function returns the peaks in the histogram
+   */
+  std::vector<cv::Point> returnPeaks();
+
+  /**
+   * @brief Function isolateLane
+   * @param peak of type cv::Point
+   * @param topView of type const cv::Mat&
+   * @return point of type std::vector
+   * The following function draws boxes on the road lanes and returns centroid points of the boxes.
+   */
+  std::vector<cv::Point> isolateLane(cv::Point peak, const cv::Mat& topView);
+
   /**
    * @brief Function computeHeadingAngle
-   * @param lines of type vector<cv::Vec4i>
-   * @return angle of type double
+   * @param boxCentroidLeft of type :vector<cv::Point>
+   * @param boxCentroidRight of type :vector<cv::Point>
+   * @return none
    * The following function will compute cars heading angle based on
-   * detected hough lines
+   * detected centroid points and connecting lines
    */
-  double computeHeadingAngle(vector<cv::Vec4i> lines);
+  void computeHeadingAngle(std::vector<cv::Point> boxCentroidLeft,
+                           std::vector<cv::Point> boxCentroidRight);
+
+  /**
+   * @brief Function returnHeadingAngle
+   * @param none
+   * @return angle of type double
+   * The following function returns the calculated heading angle
+   */
+  double returnHeadingAngle();
+
   /**
    * @brief Function laneDetection
    * @param image of type cv::Mat
@@ -142,7 +232,8 @@ class VisionModule {
    * display text on the passed image using vector of hough lines
    * and heading angle variables
    */
-  cv::Mat laneDetection(&cv::Mat);
+  cv::Mat laneDetection(cv::Mat&);
+
 };
 
 #endif /* INCLUDE_VISIONMODULE_H_ */
